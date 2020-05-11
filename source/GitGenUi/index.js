4191b3f6331582adc59c3ef5e0668c127290180c
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2020-05-11 13:47:17 -0700

    patch/refactor: [#171661740] finished config refactor - for now

--- source/GitGenUi/index.js
+++ source/GitGenUi/index.js
@@ -5,7 +5,6 @@
 .includeInNamespace(require('./GitGenUi'))
 .addModules({
   Cli:               require('./Cli'),
-  Config:            require('./Config'),
   ConfigShared:      require('./ConfigShared'),
   Git:               require('./Git'),
   GitGenUiPromptFor: require('./GitGenUiPromptFor'),
