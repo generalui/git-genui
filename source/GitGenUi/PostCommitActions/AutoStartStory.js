2c04ddea6292db3206bdaf7923210fb6a24cdb3d
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2020-06-19 12:48:57 -0700

    patch/fix: [#173159485] fixed - the value returned from autoStartStory should now be the real state

--- source/GitGenUi/PostCommitActions/AutoStartStory.js
+++ source/GitGenUi/PostCommitActions/AutoStartStory.js
@@ -2,32 +2,26 @@
 let Caf = require("caffeine-script-runtime");
 Caf.defMod(module, () => {
   return Caf.importInvoke(
-    ["log", "present", "grey", "Promise"],
+    ["present", "log", "grey", "Promise"],
     [global, require("../StandardImport"), require("colors")],
-    (log, present, grey, Promise) => {
+    (present, log, grey, Promise) => {
       return function(state) {
-        let story, id, temp, base;
-        log("autoStartStory1");
+        let story, storyState, id, temp, base;
         return (present(
           ((story = state.story),
           Caf.exists(story)
-            ? ((state = story.state), (id = story.id))
+            ? ((storyState = story.state), (id = story.id))
             : undefined)
-        ) && state === "unstarted"
-          ? (log("autoStartStory2"),
-            require("../Tracker").tracker.updateStory(story.id, {
+        ) && storyState === "unstarted"
+          ? require("../Tracker").tracker.updateStory(story.id, {
               state: "started",
               estimate: (temp = story.estimate) != null ? temp : 1
-            }))
-          : (log("autoStartStory3"),
-            Caf.exists((base = state.options)) && base.verbose
+            })
+          : (Caf.exists((base = state.options)) && base.verbose
               ? log(grey("skip: AutoStartStory (no story)"))
               : undefined,
             Promise.resolve())
-        ).then(() => {
-          log({ autoStartStory4: state });
-          return state;
-        });
+        ).then(() => state);
       };
     }
   );
