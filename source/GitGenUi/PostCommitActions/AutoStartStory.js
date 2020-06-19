a1a35975fab15bf2b86ce081d26dd598f4be1b65
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2020-06-19 12:47:39 -0700

    patch/improvement: [#173159485] hmm- why was commit-success: undefined ?

--- source/GitGenUi/PostCommitActions/AutoStartStory.js
+++ source/GitGenUi/PostCommitActions/AutoStartStory.js
@@ -2,26 +2,32 @@
 let Caf = require("caffeine-script-runtime");
 Caf.defMod(module, () => {
   return Caf.importInvoke(
-    ["present", "log", "grey", "Promise"],
+    ["log", "present", "grey", "Promise"],
     [global, require("../StandardImport"), require("colors")],
-    (present, log, grey, Promise) => {
+    (log, present, grey, Promise) => {
       return function(state) {
         let story, id, temp, base;
+        log("autoStartStory1");
         return (present(
           ((story = state.story),
           Caf.exists(story)
             ? ((state = story.state), (id = story.id))
             : undefined)
         ) && state === "unstarted"
-          ? require("../Tracker").tracker.updateStory(story.id, {
+          ? (log("autoStartStory2"),
+            require("../Tracker").tracker.updateStory(story.id, {
               state: "started",
               estimate: (temp = story.estimate) != null ? temp : 1
-            })
-          : (Caf.exists((base = state.options)) && base.verbose
+            }))
+          : (log("autoStartStory3"),
+            Caf.exists((base = state.options)) && base.verbose
               ? log(grey("skip: AutoStartStory (no story)"))
               : undefined,
             Promise.resolve())
-        ).then(() => state);
+        ).then(() => {
+          log({ autoStartStory4: state });
+          return state;
+        });
       };
     }
   );
