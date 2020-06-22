7f84bb6745a3b5312cd1c1a4d56270cc2b25a8d6
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2020-06-22 09:51:16 -0700

    patch/fix: [#173453268] ok, fixed it, now you can create new stories as part of commit

--- source/GitGenUi/Commands/CommandsLib/StoryMenu.js
+++ source/GitGenUi/Commands/CommandsLib/StoryMenu.js
@@ -2,22 +2,17 @@
 let Caf = require("caffeine-script-runtime");
 Caf.defMod(module, () => {
   return Caf.importInvoke(
-    ["log"],
+    ["log", "merge"],
     [global, require("../StandardImport")],
-    log => {
-      return require("./Lib").updateStateWithPrompt("story", function({
-        stories,
-        story
-      }) {
+    (log, merge) => {
+      return require("./Lib").updateStateWithPrompt("story", function(state) {
         log(
           `Tip: Select ${Caf.toString(
             require("colors").green("(none)")
           )} for no story. It's at the bottom of the story list.`
         );
-        return require("../../GitGenUiPromptFor").story(
-          stories,
-          "What story does this commit advance most?",
-          story
+        return require("./SelectStory")(
+          merge(state, { prompt: "What story does this commit advance most?" })
         );
       });
     }
