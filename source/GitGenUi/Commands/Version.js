faba96f998747157ea2c2af178d61dc3c500fe91
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2020-06-25 11:38:30 -0700

    patch/improvement: verbose version info

--- source/GitGenUi/Commands/Version.js
+++ source/GitGenUi/Commands/Version.js
@@ -1,10 +1,11 @@
 "use strict";
 let Caf = require("caffeine-script-runtime");
 Caf.defMod(module, () => {
+  let Neptune = global.Neptune;
   return {
     description: "show version",
-    run: function() {
-      return require("../../../package").version;
+    run: function({ verbose }) {
+      return verbose ? Neptune : require("../../../package").version;
     }
   };
 });
